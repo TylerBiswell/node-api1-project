4039c122e4c45fc39b6805ee52239fcb604622d3
Tyler Biswell <tsbiswell@gmail.com>
2019-10-01 10:17:50 -0600

    delete endpoint

--- index.js
+++ index.js
@@ -41,11 +41,6 @@ server.get('/api/users', (req, res) => {
 server.get('/api/users/:id', (req, res) => {
     const id = req.params.id;
   
-    // if (false) {
-    //   res
-    //     .status(404)
-    //     .json({ error: 'The user with the specified ID does not exist.' });
-    // } else {
     users
       .findById(id)
       .then(user => {
@@ -54,7 +49,7 @@ server.get('/api/users/:id', (req, res) => {
             .status(404)
             .json({ error: 'The user with the specified ID does not exist.' });
         } else {
-          res.send(user);
+          res.json(user);
         }
       })
       .catch(err =>
@@ -62,7 +57,23 @@ server.get('/api/users/:id', (req, res) => {
           .status(500)
           .json({ error: 'The user information could not be retrieved.' }),
       );
-    // }
+    });
+
+    server.delete('/api/users/:id', (req, res) => {
+      const id = req.params.id;
+    
+      users
+        .remove(id)
+        .then(user => {
+          if (!user) {
+            res
+              .status(404)
+              .json({ error: 'The user with the specified ID does not exist.' });
+          } else {
+            res.json(user);
+          }
+        })
+        .catch();
   });
   
 const port = 8000;
